f1d92fc25b37e1806f56a76cd028a8e4b391bda0
Alejandro Jauregui Zarate <74626412+Jaza2401@users.noreply.github.com>
2023-11-04 14:54:54 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/*
+Equipo:
+
+Integrantes:
+
+- Gustavo Betancourt Mazomenos A01252832
+
+- Alejandro Jauregui Zarate A01252368
+
+TC2038
+
+Fecha 6 de Noviembre del 2023
+
+Compilacion para ejecucion:
+ g++ -std=c++17 -O3 -o main *.cpp
+Ejecucion:
+ ./main < TestCases/test1.txt
+
+*/
+
+#include "Graphs.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+  stringstream inputInfo;
+  inputInfo << cin.rdbuf();
+
+  Graphs g1;
+  g1.readGraph(inputInfo);
+  g1.print();
+
+  return 0;
+}
