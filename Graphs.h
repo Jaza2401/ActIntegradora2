9dd5926bdab91eec4a5cbc3e3610b37125c2b2b9
Alejandro Jauregui Zarate <74626412+Jaza2401@users.noreply.github.com>
2023-11-07 18:40:20 -0700

    Update Graphs.h

--- Graphs.h
+++ Graphs.h
@@ -1,8 +1,12 @@
 #ifndef _GRAPHS_H_
 #define _GRAPHS_H_
 
+#include <algorithm>
+#include <climits>
 #include <iostream>
 #include <list>
+#include <queue>
+#include <set>
 #include <string>
 #include <utility>
 #include <vector>
@@ -17,11 +21,18 @@ private:
 
   void split(std::string line, std::vector<int> &res);
 
+  std::vector<std::list<std::pair<std::pair<int, int>, std::pair<int, int>>>>
+      levelGraph;
+  std::vector<int> level;
+
 public:
   Graphs();
   ~Graphs();
   void readGraph(std::istream &input);
   void print();
+  void KruskalMST();
+  void DinicsMaxFlow();
+  int dfs(int, int, std::vector<std::list<std::pair<int, int>>> &rg);
 };
 
 #endif // _GRAPHS_H_
